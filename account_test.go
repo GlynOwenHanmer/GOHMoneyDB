1e0ebeb8e09ebe1f8d2d176f2060376260ad78aa
Glyn Owen Hanmer <glynhanmer@gmail.com>
2018-01-15 23:47:55 +0100

    JSON Loops

--- account_test.go
+++ account_test.go
@@ -1,12 +1,13 @@
 package storage
 
 import (
+	"encoding/json"
 	"testing"
-
 	"time"
 
 	"github.com/glynternet/go-accounting/account"
 	"github.com/glynternet/go-accounting/balance"
+	"github.com/glynternet/go-money/common"
 	"github.com/glynternet/go-money/currency"
 	gtime "github.com/glynternet/go-time"
 	"github.com/stretchr/testify/assert"
@@ -14,6 +15,7 @@ import (
 
 type mockAccountAccount struct {
 	equal bool
+	json  string
 }
 
 func (a mockAccountAccount) Name() (s string)                              { return }
@@ -24,6 +26,7 @@ func (a mockAccountAccount) IsOpen() (b bool)                              { ret
 func (a mockAccountAccount) CurrencyCode() (c currency.Code)               { return }
 func (a mockAccountAccount) ValidateBalance(b balance.Balance) (err error) { return }
 func (a mockAccountAccount) Equal(b account.Account) bool                  { return a.equal }
+func (a mockAccountAccount) MarshalJSON() ([]byte, error)                  { return []byte(a.json), nil }
 
 func TestAccount_Equal(t *testing.T) {
 	// if account a is true, account.Account.Equal will evaluate to true
@@ -80,7 +83,7 @@ func TestAccount_Equal(t *testing.T) {
 		b := Account{Account: mockAccountAccount{equal: true}, deletedAt: gtime.NullTime{Valid: true}}
 		var equal bool
 		equal, err := a.Equal(b)
-		assert.True(t, !equal)
+		assert.False(t, equal)
 		assert.Error(t, err, "accounts are equal but one has been deleted")
 	})
 }
@@ -93,4 +96,56 @@ func TestDeletedAt(t *testing.T) {
 	err := DeletedAt(time)(&a)
 	assert.Nil(t, err)
 	assert.Equal(t, gtime.NullTime{Valid: true, Time: time}, a.deletedAt)
+}
+
+func TestAccount_JSONLoop(t *testing.T) {
+	c, err := currency.NewCode("NEO")
+	common.FatalIfErrorf(t, err, "creating currency code")
+
+	for _, test := range []struct {
+		name  string
+		code  currency.Code
+		open  time.Time
+		close gtime.NullTime
+	}{
+		{
+			name: "with close time",
+			open: time.Date(1999, 0, 0, 0, 0, 0, 0, time.UTC),
+			close: gtime.NullTime{
+				Valid: true,
+				Time:  time.Date(2000, 0, 0, 0, 0, 0, 0, time.UTC),
+			},
+		},
+		{
+			name: "without close time",
+			open: time.Date(1999, 0, 0, 0, 0, 0, 0, time.UTC),
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			inner, err := account.New(
+				test.name,
+				*c,
+				test.open,
+			)
+			common.FatalIfError(t, err, "creating inner account")
+			if test.close.Valid {
+				err := account.CloseTime(test.close.Time)(inner)
+				common.FatalIfError(t, err, "creating close time")
+			}
+			a := Account{
+				ID: 147827,
+				deletedAt: gtime.NullTime{
+					Valid: true,
+					Time:  time.Date(1000, 0, 0, 0, 0, 0, 0, time.UTC),
+				},
+				Account: inner,
+			}
+			bs, err := json.Marshal(a)
+			common.FatalIfError(t, err, "marshalling json")
+			var actual Account
+			err = json.Unmarshal(bs, &actual)
+			common.FatalIfError(t, err, "unmarshalling json")
+			assert.Equal(t, a, actual, "intermediate stage: %s", bs)
+		})
+	}
 }
--- balance_test.go
+++ balance_test.go
@@ -1,9 +1,12 @@
 package storage
 
 import (
+	"encoding/json"
 	"testing"
+	"time"
 
 	"github.com/glynternet/go-accounting/balance"
+	"github.com/glynternet/go-money/common"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -38,4 +41,20 @@ func TestBalance_Equal(t *testing.T) {
 			assert.Equal(t, test.equal, test.a.Equal(test.b))
 		})
 	}
+}
+
+func TestBalance_JSONLoop(t *testing.T) {
+	b := Balance{
+		ID: 47,
+		Balance: balance.Balance{
+			Date:   time.Date(1000, 0, 0, 0, 0, 0, 0, time.UTC),
+			Amount: -34567,
+		},
+	}
+	bs, err := json.Marshal(b)
+	common.FatalIfError(t, err, "marshalling Balance json")
+	var actual Balance
+	err = json.Unmarshal(bs, &actual)
+	assert.Nil(t, err)
+	assert.Equal(t, b, actual)
 }
